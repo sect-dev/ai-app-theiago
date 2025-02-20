87869d9e4f3c295ad2bab17123d1f00f81049522
nikita <nikita.ratkevich91@mail.ru>
2025-02-20 15:45:56 +0200

    fix types for useClickOutside hook

--- src/app/shared/hooks/useClickOutside.ts
+++ src/app/shared/hooks/useClickOutside.ts
@@ -1,6 +1,6 @@
 import { useEffect, RefObject } from "react";
 
-export const useClickOutside = (ref: RefObject<HTMLElement>, callback: () => void) => {
+export const useClickOutside = (ref: RefObject<HTMLElement | null>, callback: () => void) => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
