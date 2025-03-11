cdb5cc787b42b43d676bb89952fa3af733c43f90
nikita <nikita.ratkevich91@mail.ru>
2025-03-11 09:19:10 +0200

    change position to mobile header

--- src/app/widgets/Header/index.tsx
+++ src/app/widgets/Header/index.tsx
@@ -21,7 +21,7 @@ const Header = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 z-50 w-full py-[6px] md:py-[14px] md:static">
+    <div className="fixed top-0 left-0 z-50 w-full py-[6px] md:py-[14px] md:relative">
       <div className="container">
         <div className="flex justify-between items-center font-bai-jamjuree">
           <div className="flex items-center gap-[14px]">
