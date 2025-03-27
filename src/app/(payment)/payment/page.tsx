5346dbe7c3609c93c7d7748c8329284e48402593
nikita <nikita.ratkevich91@mail.ru>
2025-03-27 10:14:15 +0200

    added Suspense to payment component

--- src/app/(payment)/payment/page.tsx
+++ src/app/(payment)/payment/page.tsx
@@ -1,39 +1,41 @@
 'use client'
-import React from 'react'
+import React, { Suspense } from 'react'
 import PaymentLoading from "@/app/(payment)/payment/PaymentLoading"
 import PaymentError from "@/app/(payment)/payment/PaymentError"
 import PaymentExpired from "@/app/(payment)/payment/PaymentExpired"
-import { useSearchParams } from "next/navigation"
+import {useSearchParams} from "next/navigation";
 
 export type PaymentStatus = 'error' | 'success' | 'expiry' | 'sale'
 
-const Page = () => {
+const PaymentContent = () => {
   const searchParams = useSearchParams()
-  const params: Record<string, string> = Object.fromEntries(searchParams.entries())
+  const params = Object.fromEntries(searchParams.entries())
 
   const action = params.action
-  const status: PaymentStatus = 
-    action && ['error', 'success', 'expiry'].includes(action) 
-      ? action as PaymentStatus 
+  const status: PaymentStatus =
+    action && ['error', 'success', 'expiry', 'sale'].includes(action)
+      ? action as PaymentStatus
       : 'sale'
 
-  const renderContent = () => {
-    switch (status) {
-      case "success":
-        return <PaymentLoading status={status} />
-      case "error":
-        return <PaymentError />
-      case "expiry":
-        return <PaymentExpired />
-      default:
-        return <PaymentLoading />
-    }
+  switch (status) {
+    case "success":
+      return <PaymentLoading status={status} />
+    case "error":
+      return <PaymentError />
+    case "expiry":
+      return <PaymentExpired />
+    default:
+      return <PaymentLoading status="sale" />
   }
+}
 
+const Page = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="bg-[#121423] rounded-[24px] py-[25px] px-[25px] w-[370px] mx-auto">
-        {renderContent()}
+        <Suspense fallback={<PaymentLoading status="sale" />}>
+          <PaymentContent />
+        </Suspense>
       </div>
     </div>
   )
