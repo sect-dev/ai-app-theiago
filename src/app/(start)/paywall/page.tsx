1e75afa2dc6053e09386216b4519b2f0098bdb6f
nikita <nikita.ratkevich91@mail.ru>
2025-03-28 12:20:49 +0200

    update paywall page

--- src/app/(start)/paywall/page.tsx
+++ src/app/(start)/paywall/page.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Initpage from "@/app/flat-pages/Initpage";
-import {getPaymentPlans} from "@/app/shared/api/payment";
-import {getCharacterInfoById} from "@/app/shared/api";
+import { getPaymentPlans } from "@/app/shared/api/payment";
+import { getCharacterInfoById } from "@/app/shared/api";
+import Spinner from '@/app/widgets/Spinner';
 
+const Page = async ({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) => {
+  const { character_id } = searchParams;
 
-
-const Page = async ({searchParams}:{searchParams: Promise<{ [key: string]: string | string[] | undefined }>}) => {
-  const {character_id} = await searchParams;
-  const paymentPlans = await getPaymentPlans()
-  const character = await getCharacterInfoById(character_id ?? '8');
+  const [paymentPlans, character] = await Promise.all([
+    getPaymentPlans(),
+    getCharacterInfoById(character_id ?? '8')
+  ]);
 
   return (
-    <Initpage paymentPlans={paymentPlans} character={character} />
+    <Suspense fallback={<Spinner/>}>
+      <Initpage paymentPlans={paymentPlans} character={character} />
+    </Suspense>
   );
 };
 
