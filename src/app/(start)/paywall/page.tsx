b316b8294381c6ece2cd7b6241b6e3d712d44303
nikita <nikita.ratkevich91@mail.ru>
2025-03-28 12:58:56 +0200

    test

--- src/app/(start)/paywall/page.tsx
+++ src/app/(start)/paywall/page.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import Initpage from "@/app/flat-pages/Initpage";
-import {getPaymentPlans} from "@/app/shared/api/payment";
-import {getCharacterInfoById} from "@/app/shared/api";
+'use client'
+import React, { Suspense } from 'react'
+import Initpage from "@/app/flat-pages/Initpage"
+import { useSearchParams } from "next/navigation"
+import { getPaymentPlans } from "@/app/shared/api/payment"
+import { getCharacterInfoById } from "@/app/shared/api"
+import Spinner from '@/app/widgets/Spinner'
 
-const Page = async ({searchParams}) => {
-  const {character_id} = await searchParams;
-  const paymentPlans = await getPaymentPlans()
-  const character = await getCharacterInfoById(character_id ?? '8');
+const InitpageContent = () => {
+  const searchParams = useSearchParams()
+  const character_id = searchParams.get('character_id') || '8'
 
+  const paymentPlans = React.use(getPaymentPlans())
+  const character = React.use(getCharacterInfoById(character_id))
+
+  return <Initpage paymentPlans={paymentPlans} character={character} />
+}
+
+const Page = () => {
   return (
-    <Initpage paymentPlans={paymentPlans} character={character} />
-  );
-};
+    <Suspense fallback={<Spinner />}>
+      <InitpageContent />
+    </Suspense>
+  )
+}
 
-export default Page;
+export default Page
