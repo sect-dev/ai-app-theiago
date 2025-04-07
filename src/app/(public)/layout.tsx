d191e6abf02d94ea88d5a99bd4f6aa1b5ad9120b
nikita <nikita.ratkevich91@mail.ru>
2025-04-07 16:08:28 +0200

    remove not usedd props

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -28,7 +28,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <div className={clsx("hidden animate-fadeIn absolute left-1/2 bottom-[5vw] -translate-x-1/2 z-[10] md:block md:rounded-[24px] md:backdrop-blur-[10px] md:bg-opacity-50 md:bg-[#121423]", {
           "!hidden": isMobileChatOpen || isMenuOpen
         })}>
-          <MobileNav isMenuOpen={isMenuOpen} pathname={pathname} />
+          <MobileNav pathname={pathname} />
         </div>
         <div className={clsx("ml-auto transition-width duration-300 w-[calc(100vw-203px)] md:w-full", {
           "w-[calc(100vw-75px)]": isChatPage
