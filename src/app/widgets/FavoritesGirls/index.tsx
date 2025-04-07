485fb1819bc0857f3d3d1bf7f3de721277f5c8e5
nikita <nikita.ratkevich91@mail.ru>
2025-04-07 16:59:40 +0200

    added arrow button for homepage slider

--- src/app/widgets/FavoritesGirls/index.tsx
+++ src/app/widgets/FavoritesGirls/index.tsx
@@ -1,12 +1,15 @@
 'use client'
 import React, {FC} from 'react';
+import Image from "next/image";
 import FavoritesGirlsCard from "@/app/widgets/FavoritesGirls/FavoritesGirlsCard";
 import {Character} from "@/app/shared/api/types";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode,Autoplay } from 'swiper/modules';
+import { FreeMode,Autoplay,Navigation } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
+import clsx from "clsx";
+import ArrowNav from "@/../public/images/icons/arrow-gradient-blue.svg";
 
 interface ComponentProps {
   avatars: Character[]
@@ -24,7 +27,11 @@ const FavoritesGirls:FC<ComponentProps>  = ({avatars}) => {
         delay: 5500,
         disableOnInteraction: true,
       }}
-      modules={[Autoplay,FreeMode]}
+      modules={[Autoplay,FreeMode,Navigation]}
+      navigation={{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }}
     >
       {avatars?.map(avatar => {
         return (
@@ -33,6 +40,12 @@ const FavoritesGirls:FC<ComponentProps>  = ({avatars}) => {
           </SwiperSlide>
         )
       })}
+       <div
+         role="button"
+         aria-label="Next Slide"
+         className={clsx("swiper-button-next gradient-border before:rounded-full flex items-center justify-center shadow-custom-purple !absolute !right-[20px] -translate-y-1/2 top-1/2 z-10 !w-[40px] !h-[40px] rounded-[50%] !bg-[#121423] after:hidden lg:!w-[4.796vw] lg:!h-[4.796vw] lg:!right-[30vw] sm:!right-[2vw] sm:!w-[11.111vw] sm:!h-[11.111vw]", {})}>
+         <Image src={ArrowNav.src} width={ArrowNav.width} height={ArrowNav.height} alt="next image"/>
+       </div>
   </Swiper>
     </div>
   );
