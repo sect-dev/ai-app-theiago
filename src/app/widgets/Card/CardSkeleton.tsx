55c109a960312c46515e6e49c9dbddc96cb05d28
nikita <nikita.ratkevich91@mail.ru>
2025-04-11 08:49:17 +0200

    added types to ref

--- src/app/widgets/Card/CardSkeleton.tsx
+++ src/app/widgets/Card/CardSkeleton.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { ForwardedRef, forwardRef } from 'react';
 
-const CardSkeleton = ({ref}) => {
+
+const CardSkeleton = forwardRef<HTMLButtonElement>((_, ref: ForwardedRef<HTMLButtonElement>) => {
   return (
     <>
-      <div ref={ref} className="skeleton-shadow flex items-end relative p-[16px] h-[386px] rounded-[20px] overflow-hidden md:p-[12px] sm:h-[270px]">
+      <button ref={ref} className="skeleton-shadow flex items-end relative p-[16px] h-[386px] pointer-events-none rounded-[20px] overflow-hidden md:p-[12px] sm:h-[270px]">
         <div className="relative z-[2]">
           <div className="flex items-center gap-[2px] mb-[8px]">
             <div className="rounded-[9px] w-[45px] animate-pulse h-[18px] bg-skeleton " />
@@ -14,9 +15,9 @@ const CardSkeleton = ({ref}) => {
             <p className="bg-skeleton rounded-[8px] animate-pulse w-[110px] h-[14px] " />
           </div>
         </div>
-      </div>
+      </button>
     </>
   );
-};
+});
 
 export default CardSkeleton;
