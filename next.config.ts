bebf53cf45e79a2c051fad7811cb3ef6e8777ab3
kalpovski <kalpovskii@gmail.com>
2025-05-13 12:19:18 +0300

    remove headers from config

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,19 @@ const nextConfig: NextConfig = {
     formats: ["image/webp"],
     unoptimized: true,
   },
-  headers: async () => {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: https://*.payprocc.com https://cdn.aigo.sect.dev https://aigo.b-cdn.net https://*.googleapis.com https://*.google.com; img-src 'self' data: https://*.payprocc.com https://cdn.aigo.sect.dev https://aigo.b-cdn.net https://*.google.com https://*.googletagmanager.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.payprocc.com https://*.googletagmanager.com https://*.google.com https://*.googleapis.com; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*.payprocc.com https://identitytoolkit.googleapis.com https://*.googleapis.com https://*.google.com https://*.googletagmanager.com https://*.theaigo.com:*; frame-src 'self' https://*.theaigo.com:* https://*.payprocc.com;"
-          }
-        ]
-      }
-    ]
-  }
+  // headers: async () => {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: https://*.payprocc.com https://cdn.aigo.sect.dev https://aigo.b-cdn.net https://*.googleapis.com https://*.google.com; img-src 'self' data: https://*.payprocc.com https://cdn.aigo.sect.dev https://aigo.b-cdn.net https://*.google.com https://*.googletagmanager.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.payprocc.com https://*.googletagmanager.com https://*.google.com https://*.googleapis.com; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*.payprocc.com https://identitytoolkit.googleapis.com https://*.googleapis.com https://*.google.com https://*.googletagmanager.com https://*.theaigo.com:*; frame-src 'self' https://*.theaigo.com:* https://*.payprocc.com;"
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 };
 
 export default nextConfig;
