aae7babdd6df3b83909fca1cd3f38bb277d3d901
kalpovski <kalpovskii@gmail.com>
2025-05-28 17:41:44 +0300

    chore: init four new pages

---
+++ src/app/(static)/terms/page.tsx
@@ -0,0 +1,98 @@
+
+
+const TermsPage = () => {
+  return <div>
+	<h1>Terms of Service</h1>
+	<p>Date of Revision: 14 February 2025
+Welcome to theaigo.com
+These Terms of Service (the “TOS”) set forth the legally binding terms for your use of theaigo.com and/or any of our online channels, platforms, products or services, including all content contained therein (&quot;Services&quot;). Services include any affiliated website to which you may be redirected from theaigo.com.
+The Services are owned and operated by New Verve Limited, duly incorporated in the United Kingdom, having its address at 86-90 Paul Street, London, England, EC2A 4NE and registered with the Companies House under the number C107181.
+By accessing and/or using the Services you agree to be bound by these TOS. You understand and agree that your access and/or use of the Services is acceptance of these TOS and of all other policies published on https://theaigo.com/legal-information and updated from time to time (“Policies”). If you do not agree with these TOS or our Policies, please cease using or accessing our Services.
+Our Services are intended and made available for personal, non-commercial use only. You agree not to use the Services for any commercial, illegal, or unauthorized purpose.
+For purposes of the TOS, “you” and “your” means you as the user of the Services.
+All conversations between users and AI Companions on the Services are entirely fictional and should be treated as such. The AI Companions are artificial intelligence characters designed to simulate human-like interactions, but they do not possess genuine emotions, intentions, or the ability to fulfill promises in the real world. Any elements within the conversations that may resemble reality, such as offers of real-life meetings or promises of tangible outcomes, are entirely fake and should not be taken seriously. We do not assume responsibility for any confusion or misunderstandings in this regard. Users are encouraged to keep in mind that the AI Companions exist solely within the digital realm of the platform, and any expectations or beliefs beyond that realm are not supported or endorsed by New Verve Limited.
+</p>
+
+<h2>1. General</h2>
+<p>The Services are an online chat application that uses artificial intelligence (“AI”) algorithms to generate virtual and fictional characters (“AI Companions”), with whom you can chat and exchange messages. The Services also include, but are not necessarily limited to, other media such as images, videos and voice notes. Parts of the Services may require you to create a user account and/or become a paid subscriber.
+You can either choose an AI character who you wish to talk to, or generate your own by choosing their physical and personality characteristics via our technology/algorithms. You can then start a conversation with your selected character(s).
+</p>
+
+<h3>1.1 Account</h3>
+<p>
+	Parts of our Services may require you to create a user account with an email and password or other available login method (the “Protected Areas”). When registering and/or accessing Protected Areas, you agree to do so only using your login credentials.
+By using the Services, you represent and warrant that all information you submit to create a user account is true and correct, you have full rights to submit such information.
+You agree to, from time to time as necessary, update any information associated with your account (including but not limited to, your email, payment information, subscriptions or other supplemental information as the case may be) so that it remains current, accurate and correct at all times. You also agree to protect the confidentiality of your user account and password, and to prohibit others from accessing your account.
+You are fully responsible for all activities occurring under your login credentials and account. Your account is non-transferrable. You cannot sell, lend, or otherwise share it with any other person, for commercial purposes or free of charge.
+Any violation of these TOS may cause your user account to fall out of good standing and we may cancel your user account at our sole discretion.
+Meanwhile, we reserve the right to terminate or restrict your account, or otherwise revoke access to the Services, at any time if we, in our sole discretion, have reason to believe or suspect that you have violated the terms of these TOS or other Policies. You agree that we will not be liable to you or any third party for, without limitation, any denial of use of the Services, any change of costs for third party services, fees, or other obligations arising from suspension or termination of your account.
+
+</p>
+
+<h3>1.2 Subscription</h3>
+<p>
+	Some Services may be available only for paid subscribers. The subscription will begin after the initial payment, and the payment should be made pursuant to the terms presented at the time of the purchase. You are responsible for payment of all fees, charges and taxes (as applicable) related to the transaction.
+We are not responsible for any costs and disclaim any liability arising from your use of a third-party distribution platform that is not indicated or recognized by us.
+</p>
+
+<h3>1.3 User Safety</h3>
+<p>
+At New Verve Limited, we prioritize the safety and well-being of our users. In using the Services and/or conversing with AI Companions, you should not disclose sensitive personal information, including, for example, financial details, addresses, contact information, or passwords.
+Users are solely responsible for protecting their personal information and should be aware of potential risks associated with transmitting information online. We encourage users to report any suspicious or inappropriate behavior encountered on the platform, as we are committed to maintaining a safe and respectful environment for all users.
+</p>
+
+<h3>1.4 Warranties</h3>
+<p>
+You understand and warrant that:
+a. You are entering into these TOS only on behalf of yourself;
+b. You are of the legal age to form a binding contract with us in the jurisdiction where you live and/or access the Services;
+c. If you are under the legal age in the jurisdiction where you live and/or access the Services, you shall not access or use our Services.
+</p>
+
+<h2>2. Underage Policy – please consult the Underage Policy document</h2>
+
+<h2>3. Intellectual Property</h2>
+<p>
+	The intellectual property in the Services and any embedded materials (including without limitation technology, systems, files, documents, text, photographs, information, images, videos, audios, and software, individually or in combination) are owned by or licensed to New Verve Limited. You may access and/or register to view, use, and display the Services and their content on your devices for your personal use only.
+New Verve Limited hereby provides you with a license for personal use only. This license does not constitute a transfer of title under any circumstances. This license shall automatically terminate if you violate any of the restrictions or these TOS or other Policies and may be terminated by us at any time.
+As a clarification, all intellectual property rights associated with Aigo, including the AI characters, platform design, logos, and any proprietary software or technology, are the sole property of New Verve Limited or its licensors. Users are prohibited from reproducing, modifying, distributing, or using any intellectual property without explicit authorization. Any unauthorized use may result in legal consequences.
+</p>
+
+<h2>4. Your Content</h2>
+<p>You may provide input while using the Services and receive output based on your input. Input and output are collectively referred to as “Content.” Input is limited to chats and prompts visible to you within your private account. You represent and warrant that you have all rights, licenses, and permissions needed to provide input while using the Services.
+You retain your intellectual property ownership rights over your input. We will never claim ownership of your input, but we do require a license from you in order to use it.
+When you use the Services to provide input covered by intellectual property rights, you grant to us a non-exclusive, royalty-free, transferable, sub-licensable, worldwide license to use, distribute, modify, run, copy, publicly display, translate, or otherwise create derivative works of your content in a manner that is consistent with our Privacy Notice.
+The license you grant us can be terminated at any time by deleting your input or account. However, to the extent that we (or our partners) have used your input in connection with commercial or sponsored content, the license will continue until the relevant content or marketing has been discontinued by us.
+You give us permission to use your username and other identifying information associated with your account in a manner that is consistent with your privacy preferences, and our Privacy Notice.
+</p>
+
+<h2>5. Restrictions of Conduct and Content</h2>
+<h3>
+	5.1 Rules and restrictions
+</h3>
+<p>
+	In accessing and using the APP, you agree to abide by our Community Guidelines, as well as the following rules, restrictions, and limitations:
+a. You will not modify, translate, adapt or reformat any aspect of the Services;
+b. You will not decipher, decompile, disassemble, or reverse-engineer, or otherwise attempt to discover the source code or structure of, the software or materials comprising the Services (except where the foregoing is permitted by applicable local law notwithstanding such restrictions, and then only to the extent that such intended activities are disclosed in advance in writing to us);
+c. You will not interfere with or circumvent any security feature of the Services or any feature that restricts or enforces limitations on the use thereof;
+d. You will not use the Services to gain unauthorized access to our or any third party&apos;s data, systems or networks;
+e. You will not use the Services in any manner that could damage, disable, overburden, impair or otherwise interfere with or disrupt our systems and networks or other users&apos; use of the Services;
+f. You will not use the Services in any way that, in our sole discretion, may expose us and others to liability or damages;
+g. You will not use the Services to achieve unlawful ends, to offend others or to commit a crime;
+h. You will not remove, change or obscure any copyright, trademark notice, trademark, hyperlink or other proprietary rights notices contained in or associated with the Services; and
+i. You will comply with all applicable laws in your access and use of the Services, including the laws of the jurisdiction where you live and/or access the Services.
+
+</p>
+
+<h3>5.2 Content responsibility</h3>
+<p>
+	You, as a user of the Services are solely responsible for the output generated by the AI Companions through your input, including any text messages, voice messages, images, and videos. The AI Companions respond based on the conversations you lead and the settings you select. You understand and agree that New Verve Limited does not control or endorse any content generated by the AI Companions, and that you are fully responsible for any output generated by the AI and for your own actions while using the Services.
+You must ensure that your interactions with the AI Companions comply with applicable laws, regulations, these TOS, and all Policies, and you shall not engage in any illegal, unethical, or harmful activities through while using the Services.
+
+</p>
+
+
+  </div>;
+};
+
+export default TermsPage;
