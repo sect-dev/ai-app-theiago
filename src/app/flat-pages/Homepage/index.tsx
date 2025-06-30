c4fb44ddbe4782509602afa5842cd19eca6e4107
Andrei Kalpovski <kalpovskii@gmail.com>
2025-06-30 05:19:10 +0300

    chore: add create image banner to main page

--- src/app/flat-pages/Homepage/index.tsx
+++ src/app/flat-pages/Homepage/index.tsx
@@ -4,13 +4,13 @@ import FavoritesGirls from "@/app/widgets/FavoritesGirls";
 import { Character } from "@/app/shared/api/types";
 import CardsList from "@/app/widgets/CardsList";
 import {
-  getUserSubscriptionInfo,
-  registerUserAfterPayment,
-  signInAnonymouslyHandler,
+	getUserSubscriptionInfo,
+	registerUserAfterPayment,
+	signInAnonymouslyHandler
 } from "@/app/shared/api/auth";
 import {
-  PaymentModalType,
-  usePaymentStore,
+	PaymentModalType,
+	usePaymentStore
 } from "@/app/shared/store/paymentStore";
 import { useRouter } from "next/navigation";
 import { useSelectedCardStore } from "@/app/shared/store/publicStore";
@@ -26,216 +26,219 @@ import * as amplitude from "@amplitude/analytics-browser";
 import log from "@/app/shared/lib/logger";
 import { safeLocalStorage } from "@/app/shared/helpers";
 import * as trackdesk from "@/app/shared/lib/trackdesk";
+import CreateImageBanner from "@/app/widgets/CreateImageBanner";
 
 interface ComponentProps {
-  avatars: Character[] | null;
-  action: PaymentModalType | null;
-  characterId: string | null;
-  orderNumber: string | null;
-  product: string | null;
-  price?: string | null;
+	avatars: Character[] | null;
+	action: PaymentModalType | null;
+	characterId: string | null;
+	orderNumber: string | null;
+	product: string | null;
+	price?: string | null;
 }
 
 const HomePage: FC<ComponentProps> = ({
-  avatars,
-  action,
-  characterId,
-  orderNumber,
-  product,
-  price,
+	avatars,
+	action,
+	characterId,
+	orderNumber,
+	product,
+	price
 }) => {
-  const { setSuccessPaymentModal, setTokens, tokens } = usePaymentStore();
-  const { setSelectedCharacterId } = useSelectedCardStore();
-  const { setIsPremium } = useAuthStore();
-  const navigate = useRouter();
-  const favoriteAvatars =
-    (avatars &&
-      avatars
-        ?.filter((item) => item.top_horizontal_list_position)
-        .sort(
-          (a, b) =>
-            a.top_horizontal_list_position - b.top_horizontal_list_position,
-        )) ||
-    null;
-  const simpleAvatars =
-    (avatars &&
-      avatars
-        ?.filter((item) => item.tags)
-        .filter((item) => !item.top_horizontal_list_position)
-        .sort((a, b) => a.position - b.position)) ||
-    null;
-  const tags: string[] = Array.from(
-    new Set(simpleAvatars?.flatMap((avatar) => avatar.tags ?? [])),
-  );
+	const { setSuccessPaymentModal, setTokens, tokens } = usePaymentStore();
+	const { setSelectedCharacterId } = useSelectedCardStore();
+	const { setIsPremium } = useAuthStore();
+	const navigate = useRouter();
+	const favoriteAvatars =
+		(avatars &&
+			avatars
+				?.filter((item) => item.top_horizontal_list_position)
+				.sort(
+					(a, b) =>
+						a.top_horizontal_list_position - b.top_horizontal_list_position
+				)) ||
+		null;
+	const simpleAvatars =
+		(avatars &&
+			avatars
+				?.filter((item) => item.tags)
+				.filter((item) => !item.top_horizontal_list_position)
+				.sort((a, b) => a.position - b.position)) ||
+		null;
+	const tags: string[] = Array.from(
+		new Set(simpleAvatars?.flatMap((avatar) => avatar.tags ?? []))
+	);
 
-  const getTokens = async (
-    orderNumber: string,
-    product: string,
-  ): Promise<boolean> => {
-    try {
-      const response = await activateTokens(orderNumber);
-      if (response) {
-        const productItem = product.split("_")[0];
-        const totalTokens = +tokens + +productItem;
-        setSelectedCharacterId(characterId);
-        localStorage.setItem("tokens", totalTokens.toString());
-        setTokens(totalTokens);
-        notification.open({
-          title: "Successful purchase",
-          type: "success",
-          description: `${productItem} tokens added to your balance`,
-        });
-        return true;
-      }
-      return false;
-    } catch (error) {
-      notification.open({
-        title: "Successful purchase",
-        type: "error",
-        description: "Something went wrong while adding tokens",
-      });
-      console.log("error");
-      return false;
-    }
-  };
+	const getTokens = async (
+		orderNumber: string,
+		product: string
+	): Promise<boolean> => {
+		try {
+			const response = await activateTokens(orderNumber);
+			if (response) {
+				const productItem = product.split("_")[0];
+				const totalTokens = +tokens + +productItem;
+				setSelectedCharacterId(characterId);
+				localStorage.setItem("tokens", totalTokens.toString());
+				setTokens(totalTokens);
+				notification.open({
+					title: "Successful purchase",
+					type: "success",
+					description: `${productItem} tokens added to your balance`
+				});
+				return true;
+			}
+			return false;
+		} catch (error) {
+			notification.open({
+				title: "Successful purchase",
+				type: "error",
+				description: "Something went wrong while adding tokens"
+			});
+			console.log("error");
+			return false;
+		}
+	};
 
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    const tempToken = localStorage.getItem("tempToken");
-    let analyticsTimer: NodeJS.Timeout | undefined;
-    const locale = new URLSearchParams(window.location.search).get("locale");
-    if (locale) {
-      safeLocalStorage.set("locale", locale);
-    }
+	useEffect(() => {
+		const accessToken = localStorage.getItem("accessToken");
+		const tempToken = localStorage.getItem("tempToken");
+		let analyticsTimer: NodeJS.Timeout | undefined;
+		const locale = new URLSearchParams(window.location.search).get("locale");
+		if (locale) {
+			safeLocalStorage.set("locale", locale);
+		}
 
-    if (avatars && action && action === "subscription_success") {
-      log.debug(
-        "HomePage.tsx",
-        "subscription_success action detected, sending analytics...",
-      );
-      analyticsTimer = setTimeout(() => {
-        sendGTMEvent({
-          event: "paywall_complete_buy",
-          placement: "quiz",
-          product_name: product,
-          currency: "USD",
-          value: parseFloat(price || "0"),
-          transaction_id: orderNumber,
-        });
+		if (avatars && action && action === "subscription_success") {
+			log.debug(
+				"HomePage.tsx",
+				"subscription_success action detected, sending analytics..."
+			);
+			analyticsTimer = setTimeout(() => {
+				sendGTMEvent({
+					event: "paywall_complete_buy",
+					placement: "quiz",
+					product_name: product,
+					currency: "USD",
+					value: parseFloat(price || "0"),
+					transaction_id: orderNumber
+				});
 
-        fbq.event("Purchase", {
-          currency: "USD",
-          value: parseFloat(price || "0"),
-        });
+				fbq.event("Purchase", {
+					currency: "USD",
+					value: parseFloat(price || "0")
+				});
 
-        ym("reachGoal", "paywall_complete_buy", {
-          placement: "quiz",
-          product_name: product,
-          currency: "USD",
-          order_price: parseFloat(price || "0"),
-        });
+				ym("reachGoal", "paywall_complete_buy", {
+					placement: "quiz",
+					product_name: product,
+					currency: "USD",
+					order_price: parseFloat(price || "0")
+				});
 
-        amplitude.track("paywall_complete_buy", {
-          placement: "quiz",
-          product_name: product,
-          currency: "USD",
-          order_price: parseFloat(price || "0"),
-          domain: window.location.hostname,
-        });
+				amplitude.track("paywall_complete_buy", {
+					placement: "quiz",
+					product_name: product,
+					currency: "USD",
+					order_price: parseFloat(price || "0"),
+					domain: window.location.hostname
+				});
 
-        trackdesk.trackConversion(price || "0");
-        
-      }, 1000);
-    }
+				trackdesk.trackConversion(price || "0");
+			}, 1000);
+		}
 
-    if (!accessToken && !tempToken && !action) {
-      signInAnonymouslyHandler();
-    }
+		if (!accessToken && !tempToken && !action) {
+			signInAnonymouslyHandler();
+		}
 
-    return () => {
-      if (analyticsTimer) {
-        clearTimeout(analyticsTimer);
-        console.log("Analytics timer cleared");
-      }
-    };
-  }, []);
+		return () => {
+			if (analyticsTimer) {
+				clearTimeout(analyticsTimer);
+				console.log("Analytics timer cleared");
+			}
+		};
+	}, []);
 
-  useEffect(() => {
-    let timeout: NodeJS.Timeout | undefined;
+	useEffect(() => {
+		let timeout: NodeJS.Timeout | undefined;
 
-    const handleTokensPurchase = async () => {
-      if (action === "tokens_success" && orderNumber && product) {
-        try {
-          const success = await getTokens(orderNumber, product);
+		const handleTokensPurchase = async () => {
+			if (action === "tokens_success" && orderNumber && product) {
+				try {
+					const success = await getTokens(orderNumber, product);
 
-          if (success) {
-            const productItem = product.split("_")[0];
+					if (success) {
+						const productItem = product.split("_")[0];
 
-            timeout = setTimeout(() => {
-              sendGTMEvent({
-                event: "token_complete_buy",
-                placement: "quiz",
-                product_name: product,
-                tokens: productItem,
-                currency: "USD",
-                value: TOKENS.find((item) => item.name === product)?.price,
-                transaction_id: orderNumber,
-              });
-              // TODO: burn TOKENS with fire
-              fbq.event("Purchase", {
-                currency: "USD",
-                value: TOKENS.find((item) => item.name === product)?.price,
-              });
-              ym("reachGoal", "token_complete_buy", {
-                placement: "quiz",
-                product_name: product,
-                tokens: productItem,
-              });
-              amplitude.track("token_complete_buy", {
-                placement: "quiz",
-                product_name: product,
-                tokens: productItem,
-                domain: window.location.hostname,
-              });
-              trackdesk.trackConversion(String(TOKENS.find((item) => item.name === product)?.price || 0));
-            }, 1000);
+						timeout = setTimeout(() => {
+							sendGTMEvent({
+								event: "token_complete_buy",
+								placement: "quiz",
+								product_name: product,
+								tokens: productItem,
+								currency: "USD",
+								value: TOKENS.find((item) => item.name === product)?.price,
+								transaction_id: orderNumber
+							});
+							// TODO: burn TOKENS with fire
+							fbq.event("Purchase", {
+								currency: "USD",
+								value: TOKENS.find((item) => item.name === product)?.price
+							});
+							ym("reachGoal", "token_complete_buy", {
+								placement: "quiz",
+								product_name: product,
+								tokens: productItem
+							});
+							amplitude.track("token_complete_buy", {
+								placement: "quiz",
+								product_name: product,
+								tokens: productItem,
+								domain: window.location.hostname
+							});
+							trackdesk.trackConversion(
+								String(TOKENS.find((item) => item.name === product)?.price || 0)
+							);
+						}, 1000);
 
-            if (!characterId) {
-              navigate.push("/");
-            } else {
-              navigate.push("/chats");
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    };
+						if (!characterId) {
+							navigate.push("/");
+						} else {
+							navigate.push("/chats");
+						}
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			}
+		};
 
-    handleTokensPurchase();
+		handleTokensPurchase();
 
-    return () => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-    };
-  }, []);
+		return () => {
+			if (timeout) {
+				clearTimeout(timeout);
+			}
+		};
+	}, []);
 
-  return (
-    <div className="h-[calc(100vh-60px)] animate-fadeIn overflow-y-auto">
-      <div className="container !px-0">
-        <div className="space-y-[8px] sm:space-y-0">
-          <FavoritesGirls avatars={favoriteAvatars} />
-          <div className="rounded-l-[24px] bg-[#121423] p-[24px] md:rounded-none md:p-[16px] sm:pt-0">
-            <CardsList tags={tags} avatars={simpleAvatars} />
-            <div className="mt-[16px]">
-              <Footer />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-[calc(100vh-60px)] animate-fadeIn overflow-y-auto">
+			<div className="container !px-0">
+				<div className="space-y-[8px] sm:space-y-0">
+					<CreateImageBanner />
+					<FavoritesGirls avatars={favoriteAvatars} />
+					<div className="rounded-l-[24px] bg-[#121423] p-[24px] md:rounded-none md:p-[16px] sm:pt-0">
+						<CardsList tags={tags} avatars={simpleAvatars} />
+						<div className="mt-[16px]">
+							<Footer />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
---
+++ src/app/widgets/CreateImageBanner/index.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import ImageIconOrange from "@/../public/images/img/image-sparkling-orange.png";
+import ImageCreateImageBanner from "@/../public/images/img/create-image-banner.png";
+import Spinner from "../Spinner";
+
+const CreateImageBanner = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const router = useRouter();
+
+	const handleClick = () => {
+		setIsLoading(true);
+		router.push("/generate");
+	};
+
+	return (
+		<div className="mx-[8px]">
+			<div className="main-gradient flex h-[157px] w-full justify-between rounded-[32px] p-[20px]">
+				<div>
+					<div className="relative z-[1] mb-[16px] flex flex-col gap-[8px]">
+						<span className="text-[24px] font-bold leading-[1.2]">
+							Create image
+						</span>
+						<span className="text-[14px] font-normal leading-[1.3] opacity-80">
+							Generate a unique photo with any character
+						</span>
+					</div>
+					<button
+						onClick={handleClick}
+						className="relative z-[1] flex h-[46px] w-[131px] items-center justify-center gap-[8px] rounded-[20px] bg-[#FFFFFF] px-[16px] py-[12px]"
+					>
+						{isLoading ? (
+							<Spinner className="mx-auto h-[22px] w-[22px] rounded-[12px] border-[2px] border-[#007AFF] border-t-transparent" />
+						) : (
+							<>
+								<Image
+									src={ImageIconOrange.src}
+									alt="image icon"
+									width={22}
+									height={22}
+									className="size-[22px]"
+								/>
+								<span className="bg-main-gradient bg-clip-text text-[18px] font-bold text-transparent">
+									Try Now
+								</span>
+							</>
+						)}
+					</button>
+				</div>
+
+				{/* TODO: add image */}
+				{/* <Image
+					src={ImageCreateImageBanner.src}
+					alt="create image banner"
+					width={ImageCreateImageBanner.width}
+					height={ImageCreateImageBanner.height}
+					className=""
+				/> */}
+			</div>
+		</div>
+	);
+};
+
+export default CreateImageBanner;
