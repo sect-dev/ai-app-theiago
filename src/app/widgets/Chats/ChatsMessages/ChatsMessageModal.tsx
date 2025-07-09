8a9ab4f5b744779f7bd38fc93bd8af37266eafef
Andrei Kalpovski <kalpovskii@gmail.com>
2025-07-09 04:32:01 +0300

    chore: add request button

--- src/app/widgets/Chats/ChatsMessages/ChatsMessageModal.tsx
+++ src/app/widgets/Chats/ChatsMessages/ChatsMessageModal.tsx
@@ -1,155 +1,115 @@
 import React, { FC, useRef } from "react";
 import { useClickOutside } from "@/app/shared/hooks/useClickOutside";
 import { useRouter } from "next/navigation";
-import { IS_CLIENT } from '@/app/shared/consts';
+import { IS_CLIENT } from "@/app/shared/consts";
 
 interface PhotoItem {
-  title: string,
-  value?: string,
-  id: number,
-  image: string;
+	title: string;
+	value?: string;
+	id: number;
+	image: string;
 }
 
 const photoPrompt = [
-  {
-    title: "Breasts ",
-    value: "Show me your gorgeous naked breasts please",
-    id: 1,
-    image: "🍒",
-  },
-  {
-    title: "Butt ",
-    value: "Send me a photo of your beautiful butt please",
-    id: 2,
-    image: "🌰",
-  },
-  {
-    title: "Pussy ",
-    value:
-      "Send me a photo of how you are lying on your back and masturbating your wet dripping pussy please",
-    id: 3,
-    image: "🐱",
-  },
-  {
-    title: "Sex ",
-    value:
-      "Send me a photo how you are enjoying hot and passionate sexual intercourse in a cowgirl pose please",
-    id: 4,
-    image: "🔞",
-  },
-  {
-    title: "Anal ",
-    value: "Send me a photo of you having an intense anal doggystyle sex",
-    id: 5,
-    image: "🍆",
-  },
-  {
-    title: "Blowjob ",
-    value: "Send me a selfie how you are doing a blowjob on your knees please",
-    id: 6,
-    image: "🫦",
-  },
-  {
-    title: "Custom ",
-    value: " ",
-    id: 7,
-    image: "✏️",
-  },
+	{
+		title: "Breasts ",
+		value: "Show me a picture of your breasts",
+		id: 1,
+		image: "🍒"
+	},
+	{
+		title: "Butt ",
+		value: "Show me a picture of your butt",
+		id: 2,
+		image: "🌰"
+	},
+	{
+		title: "Pussy ",
+		value: "Show me a picture of your pussy",
+		id: 3,
+		image: "🐱"
+	},
+	{
+		title: "Custom ",
+		value: "Show me a picture of your [body part] while you [action or pose]",
+		id: 4,
+		image: "✏️"
+	}
 ];
 
 const textPrompt = [
-  {
-    title: "Send me... ",
-    id: 1,
-  },
-  {
-    title: "Show me... ",
-    id: 2,
-  },
-  {
-    title: "Send... ",
-    id: 3,
-  },
-  {
-    title: "Can i see... ",
-    id: 4,
-  },
+	{
+		title: "Send me... ",
+		id: 1
+	}
 ];
 
 interface ComponentProps {
-  closeModal: () => void;
-  onSelectMessage: (text: string) => void;
+	closeModal: () => void;
+	onSelectMessage: (text: string) => void;
 }
 
 const ChatsMessageModal: FC<ComponentProps> = ({
-  closeModal,
-  onSelectMessage,
+	closeModal,
+	onSelectMessage
 }) => {
-  const modalRef = useRef<HTMLDivElement>(null);
-  const router = useRouter();
-  useClickOutside(modalRef, closeModal);
+	const modalRef = useRef<HTMLDivElement>(null);
+	const router = useRouter();
+	useClickOutside(modalRef, closeModal);
 
-  const handleCustomClick = (item: PhotoItem, prompt: string) => {
-    if (item.title.includes("Custom")) {
-      closeModal();
-      if (IS_CLIENT) {
-        router.push("/create")
-      }
-    } else {
-      onSelectMessage(prompt)
-    }
+	const handleCustomClick = (item: PhotoItem, prompt: string) => {
+		onSelectMessage(prompt);
+	};
 
-  }
-
-  return (
-    <div
-      ref={modalRef}
-      className="chats-suggestion-modal absolute bottom-[calc(100%+10px)] right-[60px] z-[5] w-[187px] animate-fadeIn space-y-[4px]"
-    >
-      {/* <div className="rounded-b-[4px] rounded-t-[12px] bg-[#21233A] px-[6px] py-[12px]">
-        <p className="mb-[10px] text-[14px] font-semibold">Ask a photo with</p>
-        <ul className="">
-          {photoPrompt.map((item) => {
-            const prompt = item.value;
-            return (
-              <li key={item.id} className="">
-                <button
-                  onClick={() => handleCustomClick(item, prompt)}
-                  className="group flex w-full items-center gap-[8px] py-[6px] text-left"
-                >
-                  <span className="text-[14px]">{item.image}</span>
-                  <span className="transition-border border-b border-b-transparent text-[12px] font-medium duration-300 group-hover:border-b-white">
-                    {item.title}
-                  </span>
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div> */}
-      <div className="rounded-b-[12px] rounded-t-[4px] bg-[#21233A] px-[6px] py-[12px]">
-        <ul className="mb-[12px]">
-          {textPrompt.map((item) => {
-            return (
-              <li key={item.id} className="group">
-                <button
-                  onClick={() => onSelectMessage(item.title)}
-                  className="block w-full py-[6px] text-left text-[12px] font-medium"
-                >
-                  <span className="transition-border border-b border-b-transparent duration-300 group-hover:border-b-white">
-                    {item.title}
-                  </span>
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-        <p className="text-[10px] font-medium leading-[1.2em] tracking-[-0.04em] opacity-50">
-          You can request any photo by starting the message with these words.
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			ref={modalRef}
+			className="chats-suggestion-modal absolute bottom-[calc(100%+10px)] right-[60px] z-[5] w-[187px] animate-fadeIn space-y-[4px]"
+		>
+			<div className="rounded-b-[4px] rounded-t-[12px] bg-[#21233A] px-[6px] py-[12px]">
+				<p className="mb-[10px] text-[14px] font-semibold">Ask a photo with</p>
+				<ul className="">
+					{photoPrompt.map((item) => {
+						const prompt = item.value;
+						return (
+							<li key={item.id} className="">
+								<button
+									onClick={() => handleCustomClick(item, item.value)}
+									className="group flex w-full items-center gap-[8px] py-[6px] text-left"
+								>
+									<span className="text-[14px]">{item.image}</span>
+									<span className="transition-border border-b border-b-transparent text-[12px] font-medium duration-300 group-hover:border-b-white">
+										{item.title}
+									</span>
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className="rounded-b-[12px] rounded-t-[4px] bg-[#21233A] px-[6px] py-[12px]">
+				<ul className="mb-[12px]">
+					{textPrompt.map((item) => {
+						return (
+							<li key={item.id} className="group">
+								<button
+									onClick={() => onSelectMessage(item.title)}
+									className="block w-full py-[6px] text-left text-[12px] font-medium"
+								>
+									<span className="transition-border border-b border-b-transparent duration-300 group-hover:border-b-white">
+										{item.title}
+									</span>
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+				<p className="text-[10px] font-medium leading-[1.2em] tracking-[-0.04em] opacity-50">
+					You can request any photo by starting the message with these words.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default ChatsMessageModal;
