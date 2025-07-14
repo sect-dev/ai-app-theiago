99d37c7512dd18f18cad8845a9fad195667dcc7b
Andrei Kalpovski <kalpovskii@gmail.com>
2025-07-14 17:20:31 +0300

    chore: fix generator banner on the main page

--- src/app/widgets/CreateImageBanner/index.tsx
+++ src/app/widgets/CreateImageBanner/index.tsx
@@ -9,6 +9,10 @@ import ImageCreateBannerRight from "@/../public/images/img/image-create-banner-r
 import ImageCreateBannerLeft from "@/../public/images/img/image-generator-banner-left.png";
 import ImageGeneratorBannerSmall from "@/../public/images/img/image-generator-banner-small.png";
 import ImageGeneratorBannerSmallSvg from "@/../public/images/img/image-generate-banner-small.svg";
+import ImageGeneratorBannerBackground from "@/../public/images/img/image-generator-banner-background.png";
+import ImageGeneratorBannerBlueShadow from "@/../public/images/img/image-generator-banner-blue-shadow.png";
+import ImageGeneratorBannerBlue from "@/../public/images/img/image-generator-banner-blue.png";
+import ImageGeneratorBannerGirls from "@/../public/images/img/image-generator-banner-girls.png";
 
 const CreateImageBanner = () => {
 	const [isLoading, setIsLoading] = useState(false);
@@ -24,16 +28,19 @@ const CreateImageBanner = () => {
 			<div className="main-gradient flex h-[225px] w-full justify-between rounded-[32px] sm:h-[175px]">
 				<div className="relative py-[35px] pl-[64px] sm:p-[16px]">
 					<div className="relative z-[1] mb-[24px] flex flex-col gap-[8px]">
-						<span className="lgm:text-[24px] text-[38px] font-bold leading-[1.2]">
+						<span className="text-[38px] font-bold leading-[1.2] lgm:text-[24px]">
 							IMAGE GENERATOR
 						</span>
-						<span className="lgm:text-[14px] text-[20px] font-semibold leading-[1.3]">
+						<span className="block text-[20px] font-semibold leading-[1.3] lgm:text-[14px] sm:hidden">
 							Get Your Perfect AI-generated Image
+						</span>
+						<span className="hidden text-[20px] font-semibold leading-[1.3] lgm:text-[14px] sm:block">
+							Get Your Perfect <br /> AI-generated Image
 						</span>
 					</div>
 					<button
 						onClick={handleClick}
-						className="shadow-white-shadow relative z-[1] flex h-[46px] w-[131px] items-center justify-center gap-[8px] rounded-[20px] bg-[#FFFFFF] px-[16px] py-[12px]"
+						className="relative z-[1] flex h-[46px] w-[131px] items-center justify-center gap-[8px] rounded-[20px] bg-[#FFFFFF] px-[16px] py-[12px] shadow-white-shadow"
 					>
 						{isLoading ? (
 							<Spinner className="mx-auto h-[22px] w-[22px] rounded-[12px] border-[2px] border-[#007AFF] border-t-transparent" />
@@ -63,11 +70,18 @@ const CreateImageBanner = () => {
 					className="z-[5] block sm:hidden"
 				/>
 				<Image
-					src={ImageGeneratorBannerSmall.src}
+					src={ImageGeneratorBannerBlue.src}
 					alt="create image banner"
-					width={ImageGeneratorBannerSmall.width}
-					height={ImageGeneratorBannerSmall.height}
-					className="absolute right-0 hidden h-[175px] w-[343px] sm:block"
+					width={ImageGeneratorBannerBlue.width}
+					height={ImageGeneratorBannerBlue.height}
+					className="absolute right-0 hidden h-full w-full sm:block"
+				/>
+				<Image
+					src={ImageGeneratorBannerGirls.src}
+					alt="girls"
+					width={ImageGeneratorBannerGirls.width}
+					height={ImageGeneratorBannerGirls.height}
+					className="absolute right-0 z-[1] hidden h-full sm:block"
 				/>
 
 				<Image
