a12e930b9ca5ab557b96ead6aa5024aa733c93f3
Andrei Kalpovski <kalpovskii@gmail.com>
2025-08-07 17:36:22 +0300

    chore: add margin bottom

--- src/app/flat-pages/CreateCharacterPage/index.tsx
+++ src/app/flat-pages/CreateCharacterPage/index.tsx
@@ -44,7 +44,7 @@ const CreateCharacterPage = () => {
 		<div className="flex h-fit justify-center sm:h-[100vh]">
 			<div
 				ref={scrollContainerRef}
-				className="relative mt-[16px] w-[491px] overflow-x-hidden rounded-[24px] bg-[#121423] p-[32px] sm:rounded-none sm:p-[16px]"
+				className="relative mb-[32px] mt-[16px] w-[491px] overflow-x-hidden rounded-[24px] bg-[#121423] p-[32px] sm:rounded-none sm:p-[16px]"
 			>
 				{step === 1 && <CharacterType />}
 				{step === 2 && <BasicInfo />}
